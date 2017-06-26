7b5f9cb9d297bcc191b8a999d5072e281084dca9
Dominic Surrao <dsurrao@Dominics-MacBook-Pro.local>
2017-06-26 16:47:01 -0400

    WorkflowState

--- canreg/workflow.py
+++ canreg/workflow.py
@@ -4,18 +4,14 @@ class Workflow:
     starting_state
 
 class WorflowState:
+    patient
     required_information = []
     child_states = []
 
-    # processes business logic using required_information
+    # processes business logic using patient, required_information
     # to move patient to child_state
     def execute_transition(self):
         return child_state
-
-class PatientWorkflowState:
-    patient
-    workflow_state
-    provided_information = []
 
 class WorkflowController:
     def define_workflow(self):
