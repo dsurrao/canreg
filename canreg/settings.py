207cc0ae44e9db6cdcc0b587bd4fa68005db0d4b
Dominic Surrao <dsurrao@Dominics-MacBook-Pro.local>
2017-07-31 12:50:53 -0400

    settings.py

--- canreg/settings.py
+++ canreg/settings.py
@@ -123,12 +123,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-<<<<<<< HEAD
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.9/howto/static-files/
-
-=======
->>>>>>> master
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
